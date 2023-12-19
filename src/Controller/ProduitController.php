3cdac1eaf9971aeb6e74af2765a685dac7d0ebfd
BADHTW <badr_mim@outlook.fr>
2023-12-19 16:49:57 +0100

    articles similaires

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -25,17 +25,37 @@ class ProduitController extends AbstractController
         ]);
     }
 
-    #[Route('/detail/{id}', name: 'app_detail')]
-    public function detail(Product $produit): Response
+    #[Route('produit/detail/{id}', name: 'app_detail')]
+    public function detail(Product $produit, ProductRepository $productRepo): Response
     {
 
 
 
-
+        // Récupérez les produits similaires en fonction de la catégorie du produit actuel, par exemple.
+        $category = $produit->getCategories()->first(); // Récupérez la première catégorie (à adapter selon votre logique)
+        $products = $productRepo->getProductByCategory($category->getLabel()); // Obtenez les produits similaires
 
         return $this->render('produit/detail.html.twig', [
             'produit' => $produit,
+            'products' => $products,
         ]);
 
     }
+
+    #[Route('/produit/{category}', name: 'app_categorie')]
+    public function showCategorie (ProductRepository $productRepo, string $category): Response
+    {
+        $product= $productRepo->getProductByCategory($category);
+        
+    
+
+        return $this->render('produit/categorie.html.twig', [
+            'produit' => $product,
+        ]);
+
+    }
+
+
+
+
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -22,6 +22,14 @@ class ProductRepository extends ServiceEntityRepository
     }
 
 
+    public function getProductByCategory( string $categoryName){
+        return $this->createQueryBuilder('p')
+        ->innerJoin('p.categories','c')
+        ->andWhere('c.label = :categoryName')
+        ->setParameter('categoryName',$categoryName)
+        ->getQuery()
+        ->getResult();
+    }
 //    /**
 //     * @return Product[] Returns an array of Product objects
 //     */
