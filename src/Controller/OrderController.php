5d5ecd7ed826d005b745b913f83eb9000c0a9de3
Julie Aymar <62514932+Keir-oriyerdan@users.noreply.github.com>
2023-12-19 10:37:12 +0100

    résolution des conflits

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class OrderController extends AbstractController
 {
+   
     #[Route('/order', name: 'app_order')]
-    public function index(): Response
+    public function index(OrderRepository $orderRepository): Response
     {
+        
+        // Récupérer la liste des commandes (order) depuis le repository
+        $orders = $orderRepository->findAll();
+
         return $this->render('order/history.html.twig', [
-            'controller_name' => 'OrderController',
+            //'controller_name' => 'OrderController',
+            'orders' => $orders,
         ]);
     }
 }
