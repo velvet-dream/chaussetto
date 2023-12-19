3a19e7bd39897465b823ae1ff75612f4b1fede54
Lilya EMAD <lilya.emad34@icloud.com>
2023-12-19 15:28:06 +0100

    ajout d'administateur, liste d'admin, visualisation d'admin

--- src/Entity/Staff.php
+++ src/Entity/Staff.php
@@ -19,7 +19,7 @@ class Staff implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $email = null;
 
     #[ORM\Column]
-    private array $roles = [];
+    private array $roles = ['ROLE_ADMIN'];
 
     /**
      * @var string The hashed password
---
+++ src/Services/FormStaffService.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace App\Services;
+
+use App\Entity\Staff;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use App\Repository\StaffRepository;
+
+
+class FormStaffService
+{
+    private ?Staff $staff;
+
+    public function __construct(
+        private EntityManagerInterface $em,
+        private FormFactoryInterface $formFactoryInterface,
+        private StaffRepository $staffRepository)
+    {
+
+    }
+
+    public function submitForm(
+        FormInterface $formInterface,
+        Staff $staff,
+        ?Request $request) :bool 
+    {
+        if ($formInterface->isSubmitted() && $formInterface->isValid()) {
+            //if($request->request->has())
+            $this->em->persist($staff);
+            $this->em->flush();
+            return true;
+          }
+          return false;
+    }
+}
