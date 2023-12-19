9cca3f4fd05b165e426a71f0e0bc0ece18ddab8a
BADHTW <badr_mim@outlook.fr>
2023-12-19 10:34:46 +0100

    ss

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -21,6 +21,7 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
+
 //    /**
 //     * @return Product[] Returns an array of Product objects
 //     */
