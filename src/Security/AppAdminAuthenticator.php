99c2e151bd275016ac72c263742c883743ab46c3
Velvet <velvet.aubry@mailo.com>
2023-12-20 15:10:44 +0100

    j'espère ne pas avoir de conflit avec très chère Lilya

--- src/Security/AppAdminAuthenticator.php
+++ src/Security/AppAdminAuthenticator.php
@@ -49,8 +49,7 @@ class AppAdminAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_admin_dashboard'));
     }
 
     protected function getLoginUrl(Request $request): string
