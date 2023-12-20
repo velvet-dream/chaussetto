ed0d05e194d7b3dbe025e886d4aab5ba7b66e979
Lilya EMAD <lilya.emad34@icloud.com>
2023-12-20 12:00:00 +0100

    add function crud, search into the back-office

--- public/js/burger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let burgerIcon = document.getElementById('burger-icon');
-    let mobileMenu = document.getElementById('web-menu');
-
-    burgerIcon.addEventListener('click', function () {
-        mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
-    });
-});
