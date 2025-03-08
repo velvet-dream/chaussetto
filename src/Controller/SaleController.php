b784c9dd2e0fee43f81c25da5091eacb85ae1882
Velvet <velvet.aubry@mailo.com>
2025-03-08 19:07:42 +0100

    fix bug

--- src/Controller/SaleController.php
+++ src/Controller/SaleController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class SaleController extends AbstractController
 {
@@ -17,5 +15,4 @@ class SaleController extends AbstractController
             'controller_name' => 'SaleController',
         ]);
     }
-
 }
