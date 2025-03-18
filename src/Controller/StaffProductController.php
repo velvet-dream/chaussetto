9cbf265700ea0ff85e5695f00e3f42fdc9ae063e
Velvet <velvet.aubry@mailo.com>
2025-03-18 16:15:50 +0100

    better indent

--- src/Controller/StaffProductController.php
+++ src/Controller/StaffProductController.php
@@ -13,16 +13,16 @@ use App\Entity\Product;
 use App\Entity\Image;
 use App\Repository\ImageRepository;
 
-
-
-#[Route ('admin')]
+#[Route('admin')]
 class StaffProductController extends AbstractController
 {
     #[Route('/listProduct', name: 'app_list_product')]
-    public function listProduct(ProductRepository $productRepository,
-    Request $request, Security $security): Response
-    {
-        if (!$security->isGranted('ROLE_ADMIN')){
+    public function listProduct(
+        ProductRepository $productRepository,
+        Request $request,
+        Security $security
+    ): Response {
+        if (!$security->isGranted('ROLE_ADMIN')) {
             return $this->redirectToRoute('app_admin_login');
         }
         $triName = $request->query->get('triName', 'asc');
@@ -48,21 +48,21 @@ class StaffProductController extends AbstractController
 
         $form = $this->createForm(ProductFormType::class, $product);
         $form->handleRequest($request);
-        
+
         if ($form->isSubmitted() && $form->isValid()) {
             $imageFile = $form->get('image')->getData(); // Récupérer le fichier image soumis
-            
+
             // Vérifier si une image a été soumise
             if ($imageFile) {
                 // Générer un nom de fichier unique
-                $newFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME).'.'.$imageFile->guessExtension();
-    
+                $newFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME) . '.' . $imageFile->guessExtension();
+
                 // Déplacer le fichier vers le répertoire où vous souhaitez le stocker
                 $imageFile->move(
                     $this->getParameter('kernel.project_dir') . '/public/images/web/', // Remplacez 'dossier_images' par le nom de votre répertoire
                     $newFilename
                 );
-    
+
                 // Enregistrer le nom de l'image dans l'entité Product
                 $img = new Image;
                 $img->setName($newFilename);
@@ -78,13 +78,12 @@ class StaffProductController extends AbstractController
         return $this->render('staff_product/addproduct.html.twig', [
             'form' => $form,
         ]);
-
     }
 
-    #[Route('/showProduct/{id}', name : 'app_show_product')]
-    public function showProduct (?Product $product, Security $security) : Response
+    #[Route('/showProduct/{id}', name: 'app_show_product')]
+    public function showProduct(?Product $product, Security $security): Response
     {
-        if (!$security->isGranted('ROLE_ADMIN')){
+        if (!$security->isGranted('ROLE_ADMIN')) {
             return $this->redirectToRoute('app_admin_login');
         }
 
@@ -95,67 +94,63 @@ class StaffProductController extends AbstractController
     }
 
     #[Route('/updateProduct/{id}', name: 'app_update_product')]
-    public function updateProduct (
+    public function updateProduct(
         Request $request,
         Security $security,
         Product $product,
         ProductRepository $productRepository,
         ImageRepository $imageRepository,
         int $id
-    ) : Response
-    {
-        if (!$security->isGranted('ROLE_ADMIN')){
+    ): Response {
+
+        if (!$security->isGranted('ROLE_ADMIN')) {
             return $this->redirectToRoute('app_index');
         }
+
         $product = $productRepository->findProductById($id);
 
-        if ($product === null){
+        if ($product === null) {
             return $this->redirectToRoute('app_list_product');
         }
-    
+
         $form = $this->createForm(ProductFormType::class, $product);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $imageFile = $form->get('image')->getData(); // Récupérer le fichier image soumis
-            
+
             // Vérifier si une image a été soumise
             if ($imageFile) {
                 // Générer un nom de fichier unique
-                $newFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME).'.'.$imageFile->guessExtension();
-    
+                $newFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME) . '.' . $imageFile->guessExtension();
+
                 // Déplacer le fichier vers le répertoire où vous souhaitez le stocker
                 $imageFile->move(
                     $this->getParameter('kernel.project_dir') . '/assets/img/web/', // Remplacez 'dossier_images' par le nom de votre répertoire
                     $newFilename
                 );
-    
+
                 // Enregistrer le nom de l'image dans l'entité Product
                 $img = new Image;
                 $img->setName($newFilename);
                 $imageRepository->save($img);
                 $product->addImage($img);
-
-
             }
             $productRepository->save($product);
-
-
-            
         }
 
         // Ajout du champ pour afficher le nom de l'image actuelle
-    $formView = $form->createView();
-    $formView->children['image']->vars['attr']['readonly'] = true;
+        $formView = $form->createView();
+        $formView->children['image']->vars['attr']['readonly'] = true;
 
-    // Récupération du nom de la première image du produit (s'il y en a)
-    $images = $product->getImages();
-    $imageName = null;
-    if ($images && count($images) > 0) {
-        $imageName = $images[0]->getName(); // Récupérer le nom de la première image
-    }
+        // Récupération du nom de la première image du produit (s'il y en a)
+        $images = $product->getImages();
+        $imageName = null;
+        if ($images && count($images) > 0) {
+            $imageName = $images[0]->getName(); // Récupérer le nom de la première image
+        }
 
-    $formView->children['image']->vars['data'] = $imageName; // Assignation du nom de l'image au champ du formulaire
+        $formView->children['image']->vars['data'] = $imageName; // Assignation du nom de l'image au champ du formulaire
 
         return $this->render('staff_product/addproduct.html.twig', [
             'title' => 'Mise à jour d\'un produit !',
@@ -177,16 +172,14 @@ class StaffProductController extends AbstractController
             // Si le produit n'est pas trouvé, tu peux rediriger ou afficher un message d'erreur
             return $this->redirectToRoute('app_list_product');
         }
-    
+
         $productRepository->remove($product);
 
         return $this->redirectToRoute('app_gestionProduct');
 
-        return $this->render('produit/GestionProduit.html.twig', [
-            'title' => 'Mise à jour d\'un produit !',
-                'product' => $product
-        ]);
+        // return $this->render('produit/GestionProduit.html.twig', [
+        //     'title' => 'Mise à jour d\'un produit !',
+        //     'product' => $product
+        // ]);
     }
-
-    
 }
